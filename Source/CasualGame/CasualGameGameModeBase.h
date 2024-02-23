002c4bf4174f3d9fd9eace26e0972d6e1123df3f
Moqi-ui <740252408@qq.com>
2024-02-23 17:30:02 +0800

    源码版空白C++工程 --by Imrcao.company

---
+++ Source/CasualGame/CasualGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CasualGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASUALGAME_API ACasualGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
